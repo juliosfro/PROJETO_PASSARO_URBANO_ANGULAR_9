18b5ddaa79f142a7bb7ef1b0b63e9ec232055fb8
JULIO DE OLIVEIRA NETO <juliosfro@gmail.com>
2021-04-15 13:28:20 -0300

    Criacao do service de ofertas.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,26 @@
+import { Oferta } from './../models/oferta.model';
+import { OfertasService } from './../services/ofertas.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'main-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [
+    OfertasService
+  ]
 })
+
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { }
+
+  public ofertas: Oferta[] = [];
 
   ngOnInit(): void {
+    this.ofertas = this.ofertasService.getofertas();
+    console.log(this.ofertas);
   }
 
 }
