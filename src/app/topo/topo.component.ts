c8e0f83528aa58387b1284b091e550dc47d93537
JULIO DE OLIVEIRA NETO <juliosfro@gmail.com>
2021-04-22 09:43:38 -0300

    DebounceTime.

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,6 +2,8 @@ import { HttpResponse } from '@angular/common/http';
 import { Oferta } from './../models/oferta.model';
 import { OfertasService } from './../services/ofertas.service';
 import { Component, OnInit } from '@angular/core';
+import { debounceTime, switchMap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'main-topo',
@@ -13,24 +15,43 @@ export class TopoComponent implements OnInit {
 
   termoPesquisa: string = '';
   oferta: Oferta = new Oferta();
+  ofertas: HttpResponse<Oferta[]> | undefined;
+
+  private _subjectPesquisa: Subject<string> = new Subject<string>();
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit(): void {
+    /* A pesquisa aguardara o usuario por 1 segundo para ele digitar o termo */
+    this._subjectPesquisa.pipe(debounceTime(1000), switchMap((termo: string) =>
+      this.ofertasService.getPesquisaOfertasPorNome(termo)))
+      .subscribe((response: HttpResponse<Oferta[]>) => {
+        this.ofertas = response
+        console.log(response);
+      });
   }
 
   pesquisa(): void {
-    this.ofertasService.getPesquisaOfertasPorNome(this.termoPesquisa).subscribe((response: HttpResponse<Oferta[]>) => {
+    console.log(this.termoPesquisa);
+    this._subjectPesquisa.next(this.termoPesquisa);
 
-      // Estamos recebendo o HttpResponse do lado do servidor da API REST
-      const requestStatus = response.status;
-      const oferta = response.body?.shift();
-      oferta !== undefined ? this.oferta = oferta : this.oferta;
-      console.log(this.oferta.descricao_oferta);
 
-    }, error => {
-      console.log(`Não há conexão com o servidor: => ${error.status}`);
 
-    });
+
+    // this.ofertasService.getPesquisaOfertasPorNome(this.termoPesquisa).subscribe((response: HttpResponse<Oferta[]>) => {
+
+    //   // Estamos recebendo o HttpResponse do lado do servidor da API REST
+    //   const requestStatus = response.status;
+    //   /* Primeiro parametro eh a response */
+    //   const oferta = response.body?.shift();
+    //   oferta !== undefined ? this.oferta = oferta : this.oferta;
+    //   console.log(this.oferta.descricao_oferta);
+
+    // }, error => {
+    //   /* Segundo parametro eh o erro */
+    //   console.log(`Não há conexão com o servidor: => ${error.status}`);
+
+    //   /* Terceiro parametro eh a conclusao */
+    // }, () => console.log(`Fluxo de eventos completo`));
   }
 }
