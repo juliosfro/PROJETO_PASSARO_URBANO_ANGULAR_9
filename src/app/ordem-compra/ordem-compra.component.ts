545bb13cdb10c00b2d6e49c6e42298495d6288cf
JULIO DE OLIVEIRA NETO <juliosfro@gmail.com>
2021-04-26 10:51:18 -0300

    Validacao de formulario usando ngForm.

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
-  constructor() { }
+  public endereco: string = '';
+  public numero: string = '';
+  public complemento: string = '';
+  public formaPagamento: string = '';
+  public formaPagamentoEstadoAlterado: boolean = false;
+  public formEstado: string = 'disabled';
+
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  mostrarOpcaoSelecionada(): void {
+    alert(this.formaPagamento);
+  }
+
+  alterarEstadoPrimitivo(): void {
+    this.formaPagamentoEstadoAlterado = true;
+  }
+
+  countCaracterIgnoreSpace(arg: string): number {
+    const resultado = arg.split('').filter(arg => arg.trim().length).join('');
+    // alert(resultado.length);
+    return resultado.length;
   }
 
 }
