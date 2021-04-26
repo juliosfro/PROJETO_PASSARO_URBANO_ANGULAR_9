9e7fe00c520e608596d5bb50a478e9d9121f3516
JULIO DE OLIVEIRA NETO <juliosfro@gmail.com>
2021-04-26 20:18:50 -0300

    Validacao de dados do formulario.

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -11,8 +11,6 @@ export class OrdemCompraComponent implements OnInit {
   public numero: string = '';
   public complemento: string = '';
   public formaPagamento: string = '';
-  public formaPagamentoEstadoAlterado: boolean = false;
-  public formEstado: string = 'disabled';
 
 
   constructor() {
@@ -21,13 +19,6 @@ export class OrdemCompraComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  mostrarOpcaoSelecionada(): void {
-    alert(this.formaPagamento);
-  }
-
-  alterarEstadoPrimitivo(): void {
-    this.formaPagamentoEstadoAlterado = true;
-  }
 
   countCaracterIgnoreSpace(arg: string): number {
     const resultado = arg.split('').filter(arg => arg.trim().length).join('');
